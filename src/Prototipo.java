fa93037dc67cc33eb8a940c6e4aa0f320430af46
vrt0004 <vrt0004@alu.ubu.es>
2016-05-12 11:06:19 +0200

    Commit para reunión 12-05-2016

--- src/Prototipo.java
+++ src/Prototipo.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -6,11 +7,11 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -24,7 +25,12 @@ import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
 import com.google.common.collect.Maps;
-import com.hubspot.jinjava.interpret.TemplateError;
+import com.lowagie.text.Cell;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.Table;
 
 import analisis.Analisis;
 import analisis.AnalisisLALR1;
@@ -32,8 +38,11 @@ import analisis.AnalisisLL1;
 import analisis.AnalisisLR1;
 import analisis.AnalisisSLR1;
 import analisis.analisisSintactico.ascendente.Automata;
+import analisis.tabla.Tabla;
+import analisis.tabla.TablaAscendente;
+import analisis.tabla.TablaDescendente;
 import gramatica.Gramatica;
-import gramatica.NoTerminal;
+
 import gramatica.VectorSimbolos;
 import parser.ParseException;
 import parser.ParserGramatica;
@@ -41,7 +50,7 @@ import parser.ParserYacc;
 
 /**
  *
- * <b>Descripci�n</b><br>
+ * <b>Descripción</b><br>
  * Prototipo de línea de comandos utilizando clases de Burgram
  * <p>
  * <b>Detalles</b><br>
@@ -76,8 +85,11 @@ public class Prototipo {
 		CommandLineParser parser = null;
 		CommandLine cmdLine = null;
 
+		Properties systemProperties = System.getProperties();
+		systemProperties.setProperty("mustache.debug", "true");
+		systemProperties.setProperty("mustache.profile", "true");
 		///////////////////////////////////////////////////////////////////////
-		// Fase 1: Configuramos las opciones de validaci�n de entrada.
+		// Fase 1: Configuramos las opciones de validación de entrada.
 		///////////////////////////////////////////////////////////////////////
 
 		Options options = new Options();
@@ -87,7 +99,7 @@ public class Prototipo {
 		options.addOption("ca", true, "Cadena a analizar");
 		options.addOption("h", "help", false, "Imprime el mensaje de ayuda");
 
-		// No pueden aparecen las dos opciones simult�neamente.
+		// No pueden aparecen las dos opciones simultaneamente.
 		OptionGroup group = new OptionGroup();
 		group.addOption(new Option("err", "Salida est�ndar de errores"));
 		group.addOption(new Option("console", "Salida est�ndar"));
@@ -96,7 +108,7 @@ public class Prototipo {
 		try {
 
 			///////////////////////////////////////////////////////////////////////
-			// Fase 2: Parseamos la entrada con la configuraci�n establecida
+			// Fase 2: Parseamos la entrada con la configuración establecida
 			///////////////////////////////////////////////////////////////////////
 
 			parser = new DefaultParser();
@@ -112,7 +124,7 @@ public class Prototipo {
 			}
 
 			// Leemos la gramatica y el tipo de analisis. Sino existen generamos
-			// un error pues es un par�metro requerido.
+			// un error pues es un parámetro requerido.
 
 			String gramatica = cmdLine.getOptionValue("g");
 			if (gramatica == null) {
@@ -230,14 +242,12 @@ public class Prototipo {
 	private static void creaXML(Gramatica g, String gramatica, Analisis analisis, String tipoanalisis, String cadena)
 			throws IOException {
 
-		String producciones = "";
+		List<Object> producciones = new ArrayList<>();
 		List<Object> traza = new ArrayList<>();
 		List<Object> terminales = new ArrayList<>();
 		List<Object> noterminales = new ArrayList<>();
 		List<Object> TodosSimbolos = new ArrayList<>();
 		List<Object> conjuntos = new ArrayList<>();
-		List<Object> respuestasalternativas = new ArrayList<>();
-		List<Object> correcta = new ArrayList<>();
 		traza.add("Pila");
 		traza.add("Entrada");
 		traza.add("Salida");
@@ -251,6 +261,55 @@ public class Prototipo {
 		} else {
 			context.put("Cadena", cadena);
 		}
+		
+		context.put("Cadena", cadena);
+		context.put("SimboloInicio", g.getSimboloInicio());
+		
+
+		for (int i = 0; i < g.obtenerTerminales().simbolosIntroducidos(); i++) {
+			terminales.add(g.obtenerTerminales().obtenerSimbolo(i).toString());
+			TodosSimbolos.add(g.obtenerTerminales().obtenerSimbolo(i).toString());
+		}
+		TodosSimbolos.add("$");
+		for (int i = 0; i < g.obtenerNoTerminales().simbolosIntroducidos(); i++) {
+			noterminales.add(g.obtenerNoTerminales().obtenerSimbolo(i).toString());
+			TodosSimbolos.add(g.obtenerNoTerminales().obtenerSimbolo(i).toString());
+		}
+
+		context.put("Terminales", terminales);
+		context.put("NumTerminales", g.obtenerTerminales().simbolosIntroducidos());
+		context.put("NoTerminales", noterminales);
+		context.put("NumNoTerminales", g.obtenerNoTerminales().simbolosIntroducidos());
+		context.put("TodosSimbolos", TodosSimbolos);
+		context.put("First", g.obtenerFirst());
+		context.put("Follow", g.obtenerFollow());
+		context.put("Traza", traza);
+		
+		List<Object> FirstFollow = creacadenasFirtsFollow(g.first.tabla, g.follow.tabla, noterminales, terminales);
+		context.put("FirstFollow", FirstFollow);
+
+		for (int i = 0; i < g.produccionesIntroducidasGramatica(); i++) {
+			producciones.add(g.obtenerProduccionGramatica(i));
+		}
+		context.put("Producciones", producciones);
+
+		
+		
+		List<Object> RowTraza = creacadenasTraza(g, analisis, noterminales, terminales, producciones);
+
+		context.put("RowTraza", RowTraza);
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		switch (tipoanalisis) {
 
 		case "LL":
@@ -258,6 +317,10 @@ public class Prototipo {
 			context.put("LR", false);
 			context.put("SLR", false);
 			context.put("LALR", false);
+			List<TASP> RowTASP = creacadenasTASP(g, analisis, noterminales, terminales, producciones);
+
+			context.put("RowTASP", RowTASP);
+
 			break;
 		case "SLR":
 			context.put("LL", false);
@@ -277,51 +340,16 @@ public class Prototipo {
 			context.put("SLR", false);
 			context.put("LALR", false);
 
-			Automata ana = analisis.obtenerAlgoritmoAnalisis().obtenerAutomata();
-			System.out.println(ana.numeroNodosAutomata());
-			for (int i = 0; i < ana.numeroNodosAutomata(); i++) {
-				conjuntos.add(ana.obtenerNodoAutomata(i).codigo());
-			}
+			
+			//TODO
+			conjuntos=creacadenasconjuntos(analisis,producciones);
+			
 			context.put("Conjuntos", conjuntos);
 			break;
 
 		}
-		context.put("Cadena", cadena);
-		context.put("SimboloInicio", g.getSimboloInicio());
-		TodosSimbolos.add("$");
 		
-		for (int i = 0; i < g.obtenerTerminales().simbolosIntroducidos(); i++) {
-			terminales.add(g.obtenerTerminales().obtenerSimbolo(i).toString());
-			TodosSimbolos.add(g.obtenerTerminales().obtenerSimbolo(i).toString());
-		}
-		
-		for (int i = 0; i < g.obtenerNoTerminales().simbolosIntroducidos(); i++) {
-			noterminales.add(g.obtenerNoTerminales().obtenerSimbolo(i).toString());
-			TodosSimbolos.add(g.obtenerNoTerminales().obtenerSimbolo(i).toString());
-		}
-
-		context.put("Terminales", terminales);
-		context.put("NumTerminales", g.obtenerTerminales().simbolosIntroducidos());
-		context.put("NoTerminales", noterminales);
-		context.put("NumNoTerminales", g.obtenerNoTerminales().simbolosIntroducidos());
-		context.put("TodosSimbolos", TodosSimbolos);
-		context.put("First", g.obtenerFirst());
-		context.put("Follow", g.obtenerFollow());
-		context.put("Traza", traza);
-		Object value = creacadenasFirtsFollow(g.first.tabla, g.follow.tabla, noterminales, terminales);
-		context.put("FirstFollow", value);
-		context.put("RespuestasalternativasFirst", respuestasalternativas);
-
-		
-		
-		
-		
-		for (int i = 0; i < g.produccionesIntroducidasGramatica(); i++) {
-			producciones = producciones + g.obtenerProduccionGramatica(i) + "\n";
-		}
-		context.put("Producciones", producciones);
 		Mustache mustache = mf.compile("my-template2.xml");
-
 		File fichero = new File(System.getProperty("user.dir"), "fichero.xml");
 		try {
 
@@ -334,9 +362,150 @@ public class Prototipo {
 		}
 	}
 
-	private static Object creacadenasFirtsFollow(Hashtable<String, VectorSimbolos> tabla,
+	private static List<Object> creacadenasconjuntos(Analisis analisis, List<Object> producciones) {
+		// TODO Auto-generated method stub
+		List<Object> theconjuntos = new ArrayList<Object>();
+		Automata ana = analisis.obtenerAutomataAnalisis();
+		ArrayList<Object> itemmarcado= new ArrayList<Object>();
+		ArrayList<Object> simboloanticipacion= new ArrayList<Object>();
+		
+		for (int i = 0; i < ana.numeroNodosAutomata(); i++) {
+		
+			String x = ana.obtenerNodoAutomata(i).toString();
+			String e = ana.obtenerNodoAutomata(i).toStringSA();
+			
+			itemmarcado.add(e);
+			int num = ana.obtenerNodoAutomata(i).numeroElementosCjtoConfig();
+			simboloanticipacion.add(obtenersimboloanticipacion(x,num));
+			String itemmarcadocadena ;
+			//for (List<Object> j : itemmarcado) {
+				 itemmarcadocadena = obtenerMultichoice(producciones, itemmarcado);
+		//	}
+			
+			Conjunto c=new Conjunto(i, itemmarcadocadena, simboloanticipacion.clone().toString());
+			simboloanticipacion.clear();
+			itemmarcado.clear();
+			theconjuntos.add(c);
+		}
+		//System.out.println(theconjuntos);
+		return theconjuntos;
+	}
+/**
+ * Metodo que obtiene los simbolos de anticipacion
+ * @param x items del conjunto
+ * @param num numero de simbolos a obtener
+ * @return Lista con los simbolos
+ */
+	private static List<Object> obtenersimboloanticipacion(String x, int num) {
+		// TODO Comprobar los simbolos que devuelve
+		List<Object> thesimb= new ArrayList<Object>();
+		int init=0;
+		int fin=0;
+		for (int i=0;i<num;i++){
+			init = x.indexOf('{')+1;
+			fin = x.indexOf('}');
+			String sim = x.substring(init, fin);
+			thesimb.add(sim);
+			
+		}
+		
+		return thesimb;
+	}
+
+	/**
+	 * método para obtener las cadenas de la traza de la cadena introducida
+	 * 
+	 * @param g
+	 * @param analisis
+	 * @param noterminales
+	 * @param terminales
+	 * @param producciones
+	 * @return
+	 */
+	private static List<Object> creacadenasTraza(Gramatica g, Analisis analisis, List<Object> noterminales,
+			List<Object> terminales, List<Object> producciones) {
+		// TODO Auto-generated method stub
+		List<Object> thetraza= new ArrayList<Object>();
+		
+		return thetraza;
+	}
+
+	/**
+	 * 
+	 * 
+	 * Metodo para obtener las cadenas de la tabla de análisis sintáctico
+	 * predictivo
+	 * 
+	 * 
+	 * @param g
+	 *            gramatica
+	 * @param analisis
+	 *            analisis
+	 * @param noterminales
+	 *            No terminales
+	 * @param terminales
+	 *            terminales
+	 * @param producciones
+	 *            producciones
+	 * @return cadena con las cadenas de la TASP ya tratadas
+	 */
+	private static List<TASP> creacadenasTASP(Gramatica g, Analisis analisis, List<Object> noterminales,
+			List<Object> terminales, List<Object> producciones) {
+
+		List<TASP> theTASP = new ArrayList<TASP>();
+
+		Tabla tabla = analisis.obtenerTablaAnalisis();
+		TablaDescendente tabla_principal = (TablaDescendente) tabla;
+		Object elemento;
+
+		List<Object> correctas = new ArrayList<>();
+		ArrayList<Object> produccion = new ArrayList<Object>();
+		
+		for (Object j : noterminales) {
+
+			for (Object i : terminales) {
+				elemento = tabla_principal.tabla_principal.get(i).get(j);
+
+				if (elemento.toString().equals("") || elemento.toString().trim().equals(0)) {
+
+					correctas.add("---");
+					String Respuestaprod = obtenerMultichoice(producciones, correctas);
+
+					produccion.add(Respuestaprod);
+
+				} else {
+
+					correctas.add(elemento.toString());
+					String Respuestaprod = obtenerMultichoice(producciones, correctas);
+					produccion.add(Respuestaprod);
+
+				}
+				correctas.clear();
+			}
+			@SuppressWarnings("unchecked")
+			TASP e = new TASP(j.toString(), (List<Object>) produccion.clone());
+			theTASP.add(e);
+			produccion.clear();
+		}
+
+		return theTASP;
+	}
+
+	/**
+	 * Metodo para obtener las cadenas del First y del Follow
+	 * 
+	 * @param tabla
+	 *            tabla con los First
+	 * @param tabla2
+	 *            tabla con los Follow
+	 * @param noterminales
+	 *            No terminales
+	 * @param terminales
+	 *            Terminales
+	 * @return cadena con las cadenas del First y del Follow ya tratadas
+	 */
+	private static List<Object> creacadenasFirtsFollow(Hashtable<String, VectorSimbolos> tabla,
 			Hashtable<String, VectorSimbolos> tabla2, List<Object> noterminales, List<Object> terminales) {
-		// TODO
 
 		List<Object> cadena = new ArrayList<>();
 		List<Object> firts = new ArrayList<>();
@@ -344,10 +513,11 @@ public class Prototipo {
 		for (Object j : noterminales) {
 			firts.add(tabla.get(j));
 			follow.add(tabla2.get(j));
-			List<Object> alternativafirst = GeneraRespuestas(firts, terminales);
-			List<Object> alternativafollow = GeneraRespuestas(follow, terminales);
+			List<Object> alternativafirst = GeneraRespuestasAleatorias(firts, terminales);
+			List<Object> alternativafollow = GeneraRespuestasAleatorias(follow, terminales);
 			String RespuestaFirst = obtenerMultichoice(alternativafirst, firts);
 			String RespuestaFollow = obtenerMultichoice(alternativafollow, follow);
+
 			Noterminal e = new Noterminal(j.toString(), RespuestaFirst, RespuestaFollow);
 			cadena.add(e);
 			firts.clear();
@@ -365,9 +535,10 @@ public class Prototipo {
 	 * @return Cadena con el multichoice
 	 */
 	private static String obtenerMultichoice(List<Object> respuestasalternativas, List<Object> correcta) {
-		String RespuestaFirst = "{1:MULTICHOICE:=" + correcta;
+		String RespuestaFirst = "{1:MULTICHOICE:=" + correcta.get(0);
 		for (int i = 0; i < respuestasalternativas.size(); i++) {
-			RespuestaFirst = RespuestaFirst + "~" + respuestasalternativas.get(i).toString();
+			
+			RespuestaFirst = RespuestaFirst + "~" + respuestasalternativas.get(i);
 		}
 		RespuestaFirst = RespuestaFirst + "}";
 		return RespuestaFirst;
@@ -450,7 +621,7 @@ public class Prototipo {
 	 * @param terminales
 	 * @return
 	 */
-	private static List<Object> GeneraRespuestas(List<Object> correcta, List<Object> terminales) {
+	private static List<Object> GeneraRespuestasAleatorias(List<Object> correcta, List<Object> terminales) {
 		int n = terminales.size();
 		List<Object> toadd = new ArrayList<>();
 		List<Object> del1 = new ArrayList<>();
@@ -464,32 +635,24 @@ public class Prototipo {
 		List<Object> sub3 = new ArrayList<>();
 		List<Object> rs = new ArrayList<>();
 		List<Object> asets = new ArrayList<>();
-
 		toadd.addAll(terminales);
 		toadd.remove(correcta.get(0));// eliminar la correcta
 		for (int i = 0; i < toadd.size(); i++) {
 			rs.add(i);
 		}
 		Collections.shuffle(rs);
-
 		if (n > 1) {
 			del1 = terminales.subList(1, n); // delete first one
 			del2 = terminales.subList(0, n - 1); // delete last one
-
 		}
-
 		if (n > 2) {
 			int r = (int) (Math.random() * n);
-
 			del3.addAll(terminales.subList(0, r));
 			del3.addAll(terminales.subList(r + 1, n));// delete other
 		}
-
 		if (rs.size() > 1) {
-
 			add1.add(toadd.get(0));// add one
 			add2.add(toadd.get(1));// add other
-
 			add12.add(toadd.get(0));
 			add12.add(toadd.get(1));// add two
 			sub1.addAll(terminales.subList(1, n - 1));
@@ -497,7 +660,6 @@ public class Prototipo {
 			sub2.addAll(terminales.subList(1, n - 1));
 			sub2.add(toadd.get((int) rs.get(1)));
 		}
-
 		if (rs.size() > 0) {
 			sub1.clear();
 			sub1.addAll(terminales.subList(1, n - 1));
@@ -505,31 +667,26 @@ public class Prototipo {
 			// substitute first one
 		}
 		if (rs.size() > 2) {
-
 			sub3.addAll(terminales.subList(0, n - 1));
 			sub3.add(toadd.get((int) rs.get(2)));
 		}
-
 		if (n == 1) {
 
 			asets.add(add1);
 			asets.add(add12);
 			asets.add(sub1);
 			asets.add(sub2);
-
 		} else {
 			if (n == 2) {
 				asets.add(del1);
 				asets.add(del2);
 				asets.add(add1);
 				asets.add(sub1);
-
 			} else {
 				asets.add(del1);
 				asets.add(add1);
 				asets.add(del2);
 				asets.add(add2);
-
 			}
 		}
 		return asets;
@@ -559,6 +716,38 @@ public class Prototipo {
 		}
 
 		String name, firts, follow;
+
+	}
+
+	/**
+	 * Clase que permite conocer la tabla de análisis sintáctico predictivo
+	 * 
+	 * @author Victor
+	 *
+	 */
+	static class TASP {
+		TASP(String name, List<Object> produccion) {
+			this.name = name;
+			this.prod = produccion;
+
+		}
+
+		String name;
+		List<Object> prod = new ArrayList<>();
+
+	}
+	static class Conjunto {
+		
+		Conjunto(int numero,String Itemmarcado, String items) {
+			this.numero = numero;
+			this.Itemmarcado = Itemmarcado;
+			this.simboloanticipacion = items;
+
+		}
+		int numero;
+	
+		String Itemmarcado;
+		String simboloanticipacion;
 
 	}
 
